6a0fab607b239db5152273f8f25dbc80807d5ee8
100164906 <scarmccloud@gmail.com>
2022-09-16 08:45:50 -0500

    Initial commit

---
+++ src/LabVarible/Main.java
@@ -0,0 +1,29 @@
+package LabVarible;
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int intOne, intTwo;
+        //add more variables, at least 2 of each type:
+        //double, float, short, String
+
+        System.out.print("Enter an integer :: ");
+        intOne = keyboard.nextInt(2001);
+
+        System.out.print("Enter an integer :: ");
+        intTwo = keyboard.nextInt(2001);
+
+        double.out.print("Enter an Integer")
+
+
+        //add in input for all variables
+
+        System.out.println();
+        System.out.println("integer one = " + intOne );
+        System.out.println("integer two = " + intTwo );
+
+        //add output for all variables
+        }
+    }
